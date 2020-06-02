a7dcc8423daa5e9ea91432cbe50b563ba31e66e3
abigailarruda <itssabbie@gmail.com>
2020-06-02 19:15:45 -0300

    estilos da pagination

--- frontend/src/components/Pagination/index.js
+++ frontend/src/components/Pagination/index.js
@@ -1,17 +1,23 @@
 import React from "react";
 import { useRouteMatch, Link } from "react-router-dom";
+import { Circle, CircleSelected } from "./styles";
 
 export default function Input(props) {
   const numerosPagina = [];
 
   let match = useRouteMatch();
 
-  for (
-    let i = 1;
-    i <= Math.ceil(props.totalReceitas / props.receitasPorPagina);
-    i++
-  ) {
-    numerosPagina.push(i);
+  for (let i = 1; i <= 3; i++) {
+    let j = props.paginaAtual;
+    if (
+      props.paginaAtual + 2 >
+      Math.ceil(props.totalReceitas / props.receitasPorPagina)
+    ) {
+      numerosPagina.push(j - 1);
+    }
+    if (j + i - 1 <= Math.ceil(props.totalReceitas / props.receitasPorPagina)) {
+      numerosPagina.push(j + i - 1);
+    }
   }
 
   return (
@@ -20,19 +26,25 @@ export default function Input(props) {
         {numerosPagina.map(function (numero) {
           if (numero === props.paginaAtual) {
             return (
-              <li key={numero} className="page-item selected">
+              <CircleSelected
+                key={numero}
+                className="page-item d-flex justify-content-center text-center"
+              >
                 <Link to={`${match.url}/${numero}`} className="page-link">
                   {numero}
                 </Link>
-              </li>
+              </CircleSelected>
             );
           } else {
             return (
-              <li key={numero} className="page-item">
+              <Circle
+                key={numero}
+                className="page-item d-flex justify-content-center text-center"
+              >
                 <Link to={`${match.url}/${numero}`} className="page-link">
                   {numero}
                 </Link>
-              </li>
+              </Circle>
             );
           }
         })}
--- frontend/src/pages/teste/index.js
+++ frontend/src/pages/teste/index.js
@@ -1,14 +1,10 @@
 import React from "react";
-import Botao from "../../components/Botao";
 import Pagination from "../../components/Pagination";
 
 function Teste() {
   return (
-    <div className="container py-5 bg-warning">
-      <Botao cor="vermelho" texto="Botão" link="/teste" />
-      <Botao cor="transparente" texto="Botão" link="/" />
-      <Botao cor="branco" texto="Botão" link="/" />
-      <Pagination totalReceitas={16} receitasPorPagina={4} paginaAtual={1} />
+    <div className="container py-5 text-center">
+      <Pagination totalReceitas={16} receitasPorPagina={4} paginaAtual={2} />
     </div>
   );
 }
